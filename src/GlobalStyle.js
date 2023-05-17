a61faeb125db778d71065db89b52d9241280dc3e
TheProgrammerGod <suvo.ghosh.965@gmail.com>
2023-05-17 22:37:27 +0530

    Adding global styles to the app

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    *{
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap');
 
+    *{
+        padding: 0px;
+        margin: 0px;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        font-family: 'Inter', sans-serif;
+    }
+
+    body{
+        background-color: aquamarine;
     }
 `;
 
